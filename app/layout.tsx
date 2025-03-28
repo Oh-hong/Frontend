493bcef39b6fa2461baf8da6bf36700652f7c462
Oh_hong <jungi9933@naver.com>
2025-03-28 16:44:05 +0900

    Revert "build에러 테스트3"
    This reverts commit 108b6aea4068f04bcec2f1e358beed09e64341be.

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,11 +55,13 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
-    `,
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
           }}
         />
       </head>
