f17c62b2101fb4dc67e0356a5219722f75ebc2ae
Oh_hong <jungi9933@naver.com>
2025-03-28 16:50:45 +0900

    Revert "build에러 테스트4"
    This reverts commit 37b9b33633d89455789890138a8131338553cdba.

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,6 +46,24 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <html lang="ko">
       <head>
         <title>번개팅</title>
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
       </head>
       <body className="custom-cursor">
         <link rel="preconnect" href="/" />
