1193926f19af5a6117e5cd86f6b4bb08839f1f8e
Oh_hong <jungi9933@naver.com>
2025-03-28 16:37:38 +0900

    Revert "build에러 테스트2"
    This reverts commit 9ec073f6910233230ed77e6dfd3194645be08069.

--- next.config.ts
+++ next.config.ts
@@ -1,37 +1,37 @@
-import { withSentryConfig } from "@sentry/nextjs";
-import withBundleAnalyzer from "@next/bundle-analyzer";
-import type { NextConfig } from "next";
-import withPWA from "next-pwa";
+import { withSentryConfig } from '@sentry/nextjs';
+import withBundleAnalyzer from '@next/bundle-analyzer';
+import type { NextConfig } from 'next';
+import withPWA from 'next-pwa';
 
 const withBundle = withBundleAnalyzer({
-  enabled: process.env.ANALYZE === "true",
+  enabled: process.env.ANALYZE === 'true',
 });
 
 const withPWAWrapper = withPWA({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
 }) as (config: NextConfig) => NextConfig;
 
 const nextConfig: NextConfig = withBundle(
   withPWAWrapper({
     reactStrictMode: true,
     images: {
-      domains: process.env.NEXT_PUBLIC_IMAGE_DOMAINS?.split(",") || [],
+      domains: process.env.NEXT_PUBLIC_IMAGE_DOMAINS?.split(',') || [],
       remotePatterns: [
         {
-          protocol: "https",
-          hostname: "codeit-doit.s3.ap-northeast-2.amazonaws.com",
+          protocol: 'https',
+          hostname: 'codeit-doit.s3.ap-northeast-2.amazonaws.com',
         },
       ],
       minimumCacheTTL: 86400,
     },
     headers: async () => [
       {
-        source: "/api/:path*",
+        source: '/api/:path*',
         headers: [
           {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
           },
         ],
       },
@@ -39,40 +39,39 @@ const nextConfig: NextConfig = withBundle(
   }) as NextConfig,
 );
 
-// export default withSentryConfig(nextConfig, {
-//   // For all available options, see:
-//   // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+export default withSentryConfig(nextConfig, {
+  // For all available options, see:
+  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-//   org: "xeun-lab",
-//   project: "thunderting",
+  org: 'xeun-lab',
+  project: 'thunderting',
 
-//   // Only print logs for uploading source maps in CI
-//   silent: !process.env.CI,
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
 
-//   // For all available options, see:
-//   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-//   // Upload a larger set of source maps for prettier stack traces (increases build time)
-//   widenClientFileUpload: true,
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
 
-//   // Automatically annotate React components to show their full name in breadcrumbs and session replay
-//   reactComponentAnnotation: {
-//     enabled: true,
-//   },
+  // Automatically annotate React components to show their full name in breadcrumbs and session replay
+  reactComponentAnnotation: {
+    enabled: true,
+  },
 
-//   // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-//   // This can increase your server load as well as your hosting bill.
-//   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-//   // side errors will fail.
-//   tunnelRoute: "/monitoring",
+  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  // This can increase your server load as well as your hosting bill.
+  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+  // side errors will fail.
+  tunnelRoute: '/monitoring',
 
-//   // Automatically tree-shake Sentry logger statements to reduce bundle size
-//   disableLogger: true,
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
 
-//   // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-//   // See the following for more information:
-//   // https://docs.sentry.io/product/crons/
-//   // https://vercel.com/docs/cron-jobs
-//   automaticVercelMonitors: true,
-// });
-export default nextConfig;
+  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+  // See the following for more information:
+  // https://docs.sentry.io/product/crons/
+  // https://vercel.com/docs/cron-jobs
+  automaticVercelMonitors: true,
+});
