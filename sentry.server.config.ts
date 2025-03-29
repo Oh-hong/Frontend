578ad9c7aa12eccbc73fb8dc14f48e14125605c4
Oh_hong <jungi9933@naver.com>
2025-03-29 16:50:07 +0900

    Revert "sentry 세팅"
    This reverts commit f70302bfea9a9af4349c99aed2e8a0622d228efc.

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,15 +1,6 @@
-// This file configures the initialization of Sentry on the server.
-// The config you add here will be used whenever the server handles a request.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: "https://128f5776d82dbed799c9c515de757e17@o4509059853123584.ingest.us.sentry.io/4509059854827520",
-
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+  dsn: process.env.SENTRY_DSN,
+  tracesSampleRate: 0,
 });
